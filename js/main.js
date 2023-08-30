ef86883d7a7aec452bc39dbf036e326272651281
miltonbanze <mfbanze@gmail.com>
2023-08-30 19:16:49 +0200

    obtenção dos dados de temperatura a partir da API

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+//Key
+const key = "9f359f49c9b6d95add5ac964421e3fc0";
+
 //Definindo a data
 const date = new Date;
 const hour = document.querySelectorAll('.top .hours');
@@ -10,8 +13,19 @@ function updateClock() {
     minutes.forEach(m => m.innerText = date.getMinutes());
     seconds.forEach(s => s.innerText = date.getSeconds());
 }
-
 updateClock();
-
 setInterval(updateClock, 1000);
 
+
+// Obtendo a cidade
+const city = document.querySelectorAll('main section h2');
+
+async function searchCity(capital) {
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${key}&lang=pt&units=metric`).then(response => response.json());
+    console.log(data);
+}
+//Cidades
+city.forEach((town, index) => {
+    const targetCity = town.innerText;
+    searchCity(targetCity); // Chamando a função para cada cidade
+});
